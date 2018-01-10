a543dee443949d58434db5ca13f9fbdc7c54600f
Rastorguev <rastorguevs@gmail.com>
2018-01-11 00:04:10 +0300

    Fix EMA calculation logic

--- BinanceTrader.Trader/MAAnalyzer.cs
+++ BinanceTrader.Trader/MAAnalyzer.cs
@@ -19,60 +19,28 @@ namespace BinanceTrader
             var prices = candles.Select(c => (decimal?) c.ClosePrice).ToList();
             var shortSMAs = CalculateSMAs(prices, shortPeriod);
             var longSMAs = CalculateSMAs(prices, longPeriod);
-            var shortEMAs = CalculateEMAs(prices, shortSMAs, shortPeriod);
-            var longEMAs = CalculateEMAs(prices, longSMAs, longPeriod);
+            var shortEMAs = CalculateEMAs(prices, shortPeriod);
+            var longEMAs = CalculateEMAs(prices, longPeriod);
             var macds = CalculateMACDs(shortEMAs, longEMAs);
-            var signals = CalculateSMAs(macds, signalPeriod);
+            var signals = CalculateEMAs(macds, signalPeriod);
 
-            //var signal = CalculateSMAs(macd, signalPeriod);
-            var trendPoints = new List<MATrend>();
-
-            for (var i = 0; i < candles.Count; i++)
-            {
-                trendPoints.Add(new MATrend
+            return candles.Select((t, i) => new MATrend
                 {
-                    OpenTime = candles[i].NotNull().OpenTime,
-                    Price = candles[i].NotNull().OpenPrice,
+                    OpenTime = t.NotNull().OpenTime,
+                    Price = t.NotNull().ClosePrice,
                     ShortSMA = shortSMAs[i],
                     ShortEMA = shortEMAs[i],
                     LongSMA = longSMAs[i],
                     LongEMA = longEMAs[i],
                     MACD = macds[i],
-                    Signals = signals[i]
-                });
-            }
-
-            //longMA = CalculateEMAs(longMA, longPeriod);
-            //shortMA = CalculateEMAs(shortMA, shortPeriod);
-
-            //shortMA = shortMA.GetRange(longPeriod - shortPeriod, longMA.Count);
-            //var maCandles = candles.GetRange(longPeriod - 1, longMA.Count);
-            //var trendPoints = new List<MATrend>();
-
-            //for (var i = 0; i < longMA.Count; i++)
-            //{
-            //    var longMAPoint = longMA[i];
-            //    var shortMAPoint = shortMA[i].NotNull();
-            //    var current = new MATrend
-            //    {
-            //        OpenTime = maCandles[i].NotNull().OpenTime,
-            //        Price = maCandles[i].NotNull().OpenPrice,
-            //        ShortSMA = shortMAPoint.SMA,
-            //        ShortEMA = shortMAPoint.EMA,
-            //        LongSMA = longMAPoint.SMA,
-            //        LongEMA = longMAPoint.EMA
-            //    };
-
-            //    trendPoints.Add(current);
-            //}
-
-            //SetPointsType(trendPoints);
-
-            return trendPoints;
+                    Signal = signals[i]
+                })
+                .ToList();
         }
 
         [NotNull]
-        private static List<decimal?> CalculateMACDs([NotNull] List<decimal?> shortEMAs,
+        private static List<decimal?> CalculateMACDs(
+            [NotNull] List<decimal?> shortEMAs,
             [NotNull] List<decimal?> longEMAs)
         {
             var macd = new List<decimal?>();
@@ -85,7 +53,6 @@ namespace BinanceTrader
                 {
                     macd.Add(null);
                 }
-
                 else
                 {
                     macd.Add(shortEMA.Value - longEMA.Value);
@@ -94,45 +61,6 @@ namespace BinanceTrader
 
             return macd;
         }
-
-        //private static void SetPointsType([NotNull] [ItemNotNull] IReadOnlyList<MATrend> trendPoints)
-        //{
-        //    for (var i = 1; i < trendPoints.Count - 1; i++)
-        //    {
-        //        var current = trendPoints[i].NotNull();
-        //        var prev = i > 0 ? trendPoints[i - 1].NotNull() : null;
-        //        var next = i < trendPoints.Count - 1 ? trendPoints[i + 1] : null;
-
-        //        if (prev != null &&
-        //            next != null &&
-        //            prev.TrendStrength < 0 &&
-        //            next.TrendStrength > 0 &&
-        //            current.TrendStrength >= 0)
-        //        {
-        //            current.Type = MATrendType.BullishCrossover;
-        //        }
-        //        else if (prev != null &&
-        //                 next != null &&
-        //                 prev.TrendStrength > 0 &&
-        //                 next.TrendStrength < 0 &&
-        //                 current.TrendStrength <= 0)
-        //        {
-        //            current.Type = MATrendType.BearishCrossover;
-        //        }
-        //        else if (current.TrendStrength > 0)
-        //        {
-        //            current.Type = MATrendType.Bullish;
-        //        }
-        //        else if (current.TrendStrength < 0)
-        //        {
-        //            current.Type = MATrendType.Bearish;
-        //        }
-        //        else if (current.TrendStrength == 0)
-        //        {
-        //            current.Type = MATrendType.Neutral;
-        //        }
-        //    }
-        //}
 
         [NotNull]
         private static List<decimal?> CalculateSMAs([NotNull] List<decimal?> values, int period)
@@ -144,122 +72,82 @@ namespace BinanceTrader
                 if (i < period - 1 || values[i] == null)
                 {
                     smas.Add(null);
+
+                    continue;
                 }
-                else
+
+                var range = values.GetRange(i - period + 1, period);
+                if (range.Any(v => v == null))
                 {
-                    var range = values.GetRange(i - period + 1, period);
+                    smas.Add(null);
 
-                    if (range.Any(v => v == null))
-                    {
-                        smas.Add(null);
-                    }
-                    else
-                    {
-                        var sma = range.Average(p => p.Value).Round();
-
-                        smas.Add(sma);
-                    }
+                    continue;
                 }
+
+                var sma = range.Average(p => p.Value).Round();
+                smas.Add(sma);
             }
 
             return smas;
         }
 
         [NotNull]
-        private static List<decimal?> CalculateEMAs(
-            [NotNull] List<decimal?> values,
-            [NotNull] List<decimal?> smas,
-            int period)
+        private static List<decimal?> CalculateEMAs([NotNull] List<decimal?> values, int period)
         {
             var emas = new List<decimal?>();
-            var k = 2 / ((decimal) (period + 1)).Round();
+            var k = 2 / (decimal) (period + 1);
 
             for (var i = 0; i < values.Count; i++)
             {
-                if (i < period - 1 || values[i] == null)
+                if (i == 0)
+                {
+                    emas.Add(values[i]);
+
+                    continue;
+                }
+
+                var current = values[i];
+                var prev = emas[i - 1];
+
+                if (current == null || prev == null)
                 {
                     emas.Add(null);
-                }
-                else
-                {
-                    if (i == period - 1)
-                    {
-                        emas.Add(smas[i]);
-                    }
-                    else
-                    {
-                        var prev = emas[i - 1].Value;
 
-                        //EMA = Price(t) * k + EMA(y) * (1 – k)
-                        //t = today, y = yesterday, N = number of days in EMA, k = 2 / (N + 1)
-                        var ema = (values[i].Value * k + prev * (1 - k)).Round();
-
-                        emas.Add(ema);
-                    }
+                    continue;
                 }
+
+                //EMA = Price(t) * k + EMA(y) * (1 – k)
+                //t = today, y = yesterday, N = number of days in EMA, k = 2 / (N + 1)
+                var ema = (current.Value * k + emas[i - 1].Value * (1 - k)).Round();
+                emas.Add(ema);
             }
 
             return emas;
         }
-
-        private class MAPoint
-        {
-            public DateTime OpenTime { get; set; }
-            public decimal ClosePrice { get; set; }
-            public decimal SMA { get; set; }
-            public decimal EMA { get; set; }
-        }
     }
+}
 
-    public class MATrend
+public class MATrend
+{
+    public DateTime OpenTime { get; set; }
+    public decimal Price { get; set; }
+    public decimal? ShortSMA { get; set; }
+    public decimal? ShortEMA { get; set; }
+    public decimal? LongSMA { get; set; }
+    public decimal? LongEMA { get; set; }
+    public decimal? MACD { get; set; }
+    public decimal? Signal { get; set; }
+
+    public decimal? MACDGist
     {
-        public DateTime OpenTime { get; set; }
-        public decimal Price { get; set; }
-        public decimal? ShortSMA { get; set; }
-        public decimal? ShortEMA { get; set; }
-        public decimal? LongSMA { get; set; }
-        public decimal? LongEMA { get; set; }
-
-        public MATrendType Type { get; set; }
-        //public decimal TrendStrength => (ShortSMA - LongSMA) * 100 / ShortSMA;
-
-        public decimal? MACD { get; set; }
-        public decimal? Signals { get; set; }
-
-        public decimal? MACDGist
+        get
         {
-            get
+            if (MACD != null && Signal != null)
             {
-                if (MACD != null && Signals != null)
-                {
-                    return MACD.Value - Signals.Value;
-                }
-
-                return null;
+                return MACD.Value - Signal.Value;
             }
+
+            return null;
         }
-
-        //public decimal? MACD
-        //{
-        //    get
-        //    {
-        //        if (ShortEMA != null && LongEMA != null)
-        //        {
-        //            return ShortEMA.Value - LongEMA.Value;
-        //        }
-
-        //        return null;
-        //    }
-        //}
-    }
-
-    public enum MATrendType
-    {
-        Undefined,
-        Neutral,
-        Bullish,
-        BullishCrossover,
-        Bearish,
-        BearishCrossover
     }
 }
--- BinanceTrader.Trader/PriceMonitor.cs
+++ BinanceTrader.Trader/PriceMonitor.cs
@@ -22,76 +22,78 @@ namespace BinanceTrader
         public void Start()
         {
             var now = DateTime.Now;
-            var candles = LoadCandles("WTC", "ETH", new DateTime(2018, 01, 10, 8, 0, 0), now, CandlesInterval.Minutes1);
+            var candles = LoadCandles("WTC", "ETH", new DateTime(2018, 01, 10, 19, 0, 0), now, CandlesInterval.Minutes1);
             var trends = candles.DefineMATrends(12, 26, 9);
 
-            var x33 = trends.First(t => t.NotNull().OpenTime == new DateTime(2018, 01, 10, 8, 33, 0));
-            var x34 = trends.First(t => t.NotNull().OpenTime == new DateTime(2018, 01, 10, 8, 34, 0));
-            var x35 = trends.First(t => t.NotNull().OpenTime == new DateTime(2018, 01, 10, 8, 35, 0));
-            var x36 = trends.First(t => t.NotNull().OpenTime == new DateTime(2018, 01, 10, 8, 36, 0));
-            var x39 = trends.First(t => t.NotNull().OpenTime == new DateTime(2018, 01, 10, 8, 39, 0));
-            var x40 = trends.First(t => t.NotNull().OpenTime == new DateTime(2018, 01, 10, 8, 40, 0));
-            var x50 = trends.First(t => t.NotNull().OpenTime == new DateTime(2018, 01, 10, 8, 50, 0));
-            var x52 = trends.First(t => t.NotNull().OpenTime == new DateTime(2018, 01, 10, 8, 52, 0));
+            var s = "";
+
+            //var x33 = trends.First(t => t.NotNull().OpenTime == new DateTime(2018, 01, 10, 8, 33, 0));
+            //var x34 = trends.First(t => t.NotNull().OpenTime == new DateTime(2018, 01, 10, 8, 34, 0));
+            //var x35 = trends.First(t => t.NotNull().OpenTime == new DateTime(2018, 01, 10, 8, 35, 0));
+            //var x36 = trends.First(t => t.NotNull().OpenTime == new DateTime(2018, 01, 10, 8, 36, 0));
+            //var x39 = trends.First(t => t.NotNull().OpenTime == new DateTime(2018, 01, 10, 8, 39, 0));
+            //var x40 = trends.First(t => t.NotNull().OpenTime == new DateTime(2018, 01, 10, 8, 40, 0));
+            //var x50 = trends.First(t => t.NotNull().OpenTime == new DateTime(2018, 01, 10, 8, 50, 0));
+            //var x52 = trends.First(t => t.NotNull().OpenTime == new DateTime(2018, 01, 10, 8, 52, 0));
 
 
-            var x08 = trends.First(t => t.NotNull().OpenTime == new DateTime(2018, 01, 10, 9, 08, 0));
-            var x09 = trends.First(t => t.NotNull().OpenTime == new DateTime(2018, 01, 10, 9, 09, 0));
-            var x10 = trends.First(t => t.NotNull().OpenTime == new DateTime(2018, 01, 10, 9, 10, 0));
-            var x11 = trends.First(t => t.NotNull().OpenTime == new DateTime(2018, 01, 10, 9, 11, 0));
-            var x12 = trends.First(t => t.NotNull().OpenTime == new DateTime(2018, 01, 10, 9, 12, 0));
-            var x13 = trends.First(t => t.NotNull().OpenTime == new DateTime(2018, 01, 10, 9, 13, 0));
-            var x14 = trends.First(t => t.NotNull().OpenTime == new DateTime(2018, 01, 10, 9, 14, 0));
-
-    
+            //var x08 = trends.First(t => t.NotNull().OpenTime == new DateTime(2018, 01, 10, 9, 08, 0));
+            //var x09 = trends.First(t => t.NotNull().OpenTime == new DateTime(2018, 01, 10, 9, 09, 0));
+            //var x10 = trends.First(t => t.NotNull().OpenTime == new DateTime(2018, 01, 10, 9, 10, 0));
+            //var x11 = trends.First(t => t.NotNull().OpenTime == new DateTime(2018, 01, 10, 9, 11, 0));
+            //var x12 = trends.First(t => t.NotNull().OpenTime == new DateTime(2018, 01, 10, 9, 12, 0));
+            //var x13 = trends.First(t => t.NotNull().OpenTime == new DateTime(2018, 01, 10, 9, 13, 0));
+            //var x14 = trends.First(t => t.NotNull().OpenTime == new DateTime(2018, 01, 10, 9, 14, 0));
 
 
-            var crossovers = trends.Where(t =>
-                t.NotNull().Type == MATrendType.BearishCrossover ||
-                t.Type == MATrendType.BullishCrossover
-            ).ToList();
 
-            const decimal fluctuation = 1m;
 
-            var ta = new MockTradingAccount(0, 1, 0, 0.1m);
-            const decimal minQuoteAmount = 0.01m;
+            //var crossovers = trends.Where(t =>
+            //    t.NotNull().Type == MATrendType.BearishCrossover ||
+            //    t.Type == MATrendType.BullishCrossover
+            //).ToList();
 
-            for (var i = 0; i < trends.Count; i++)
-            {
-                var point = trends[i].NotNull();
-                var prev = i - 1 > 0 ? trends[i - 1] : null;
+            //const decimal fluctuation = 1m;
 
-                if (prev != null && prev.Type == MATrendType.BearishCrossover)
-                {
-                    var baseAmount = Math.Floor(ta.CurrentBaseAmount);
+            //var ta = new MockTradingAccount(0, 1, 0, 0.1m);
+            //const decimal minQuoteAmount = 0.01m;
 
-                    if (baseAmount > 0 &&
-                        point.Price > ta.LastPrice + ta.LastPrice.Percents(fluctuation))
-                    {
-                        ta.Sell(baseAmount, point.Price);
-                        LogOrder("Sell", ta, point);
-                    }
-                }
-                else if (prev != null &&
-                         prev.Type == MATrendType.BullishCrossover &&
-                         ta.CurrentQuoteAmount > minQuoteAmount)
-                {
-                    var baseAmount = Math.Floor(ta.CurrentQuoteAmount / point.Price);
-                    if (baseAmount > 0)
-                    {
-                        ta.Buy(baseAmount, point.Price);
-                        LogOrder("Buy", ta, point);
-                    }
-                }
-            }
+            //for (var i = 0; i < trends.Count; i++)
+            //{
+            //    var point = trends[i].NotNull();
+            //    var prev = i - 1 > 0 ? trends[i - 1] : null;
 
-            var initialAmount = ta.InitialBaseAmount * ta.InitialPrice + ta.InitialQuoteAmount;
-            var currentAmount = ta.CurrentBaseAmount * ta.LastPrice + ta.CurrentQuoteAmount;
-            var profit = MathUtils.CalculateProfit(
-                initialAmount,
-                currentAmount).Round();
+            //    if (prev != null && prev.Type == MATrendType.BearishCrossover)
+            //    {
+            //        var baseAmount = Math.Floor(ta.CurrentBaseAmount);
 
-            Console.WriteLine($"Profit {profit}");
+            //        if (baseAmount > 0 &&
+            //            point.Price > ta.LastPrice + ta.LastPrice.Percents(fluctuation))
+            //        {
+            //            ta.Sell(baseAmount, point.Price);
+            //            LogOrder("Sell", ta, point);
+            //        }
+            //    }
+            //    else if (prev != null &&
+            //             prev.Type == MATrendType.BullishCrossover &&
+            //             ta.CurrentQuoteAmount > minQuoteAmount)
+            //    {
+            //        var baseAmount = Math.Floor(ta.CurrentQuoteAmount / point.Price);
+            //        if (baseAmount > 0)
+            //        {
+            //            ta.Buy(baseAmount, point.Price);
+            //            LogOrder("Buy", ta, point);
+            //        }
+            //    }
+            //}
+
+            //var initialAmount = ta.InitialBaseAmount * ta.InitialPrice + ta.InitialQuoteAmount;
+            //var currentAmount = ta.CurrentBaseAmount * ta.LastPrice + ta.CurrentQuoteAmount;
+            //var profit = MathUtils.CalculateProfit(
+            //    initialAmount,
+            //    currentAmount).Round();
+
+            //Console.WriteLine($"Profit {profit}");
         }
 
         [NotNull]
@@ -124,7 +126,7 @@ namespace BinanceTrader
             var ca = ta.CurrentBaseAmount * ta.LastPrice + ta.CurrentQuoteAmount;
 
             Console.WriteLine(action);
-            Console.WriteLine($"Trend :{point.Type}");
+            //Console.WriteLine($"Trend :{point.Type}");
             Console.WriteLine(point.OpenTime);
             Console.WriteLine($"Price: {ta.LastPrice}");
             Console.WriteLine($"Base amount: {ta.CurrentBaseAmount}");
