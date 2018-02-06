64c2f7cf28bbe982d2710f1094eac849dba12189
Rastorguev <rastorguevs@gmail.com>
2018-02-06 23:03:03 +0300

    Add test code to Trader

--- BinanceTrader.Trader/Trader.cs
+++ BinanceTrader.Trader/Trader.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading;
+﻿using System;
+using System.Linq;
+using System.Timers;
 using Binance.API.Csharp.Client;
 using Binance.API.Csharp.Client.Models.Enums;
 using BinanceTrader.Api;
@@ -8,31 +10,69 @@ namespace BinanceTrader
 {
     public class Trader
     {
-        public void Trade()
+        private readonly Timer _timer;
+        private readonly BinanceClient _binanceClient;
+
+        public Trader()
         {
             var keyProvider = new BinanceKeyProvider(@"D:/Keys.config");
             var keys = keyProvider.GetKeys().NotNull();
             var apiClient = new ApiClient(keys.ApiKey, keys.SecretKey);
-            var binanceClient = new BinanceClient(apiClient);
+            _binanceClient = new BinanceClient(apiClient, true);
 
-            var prices = binanceClient.GetAllPrices().Result;
-            var info = binanceClient.GetAccountInfo().Result;
-            var priceChangeInfo = binanceClient.GetPriceChange24H("TRXETH").Result;
+            _timer = new Timer { Interval = TimeSpan.FromSeconds(1).TotalMilliseconds, AutoReset = true };
+            _timer.Elapsed += OnTimerElapsed;
+            _timer.Start();
+        }
 
-            var id = Thread.CurrentThread.ManagedThreadId;
+        public void Trade()
+        {
+            //var prices = _binanceClient.GetAllPrices().Result;
+            //var info = _binanceClient.GetAccountInfo().Result;
+            //var priceChangeInfo = _binanceClient.GetPriceChange24H("TRXETH").Result;
+
+            //var id = Thread.CurrentThread.ManagedThreadId;
+
+            //var orders1 = _binanceClient.GetCurrentOpenOrders().Result;
+
+            //var listenKey = _binanceClient.ListenUserDataEndpoint(
+            //    accountUpdatedMessage => { },
+            //    orderUpdatedMessage => { },
+            //    orderUpdatedMessage =>
+            //    {
+            //        var orders = _binanceClient.GetCurrentOpenOrders().Result;
+            //    });
+
+            //var newOrder = _binanceClient.PostNewOrder("ADAETH", 10, 90, OrderSide.SELL).Result;
+        }
+
+        public void Start()
+        {
+        }
+
+        public async void OnTimerElapsed(object sender, ElapsedEventArgs elapsedEventArgs)
+        {
+           
+            var dt = DateTime.Now.ToLocalTime();
+            var t = await _binanceClient.GetServerTime();
+            var time = DateTimeOffset.FromUnixTimeMilliseconds(t.ServerTime).LocalDateTime;
+            var dt1 = DateTime.Now.ToLocalTime();
 
 
-            var orders1 = binanceClient.GetCurrentOpenOrders().Result;
+            var symbol = "ADAETH";
+            var openOrders = await _binanceClient.GetCurrentOpenOrders();
+            var trxOrders = openOrders.Where(o => o.Symbol == symbol);
 
-            var listenKey = binanceClient.ListenUserDataEndpoint(
-                accountUpdatedMessage => { },
-                orderUpdatedMessage => { },
-                orderUpdatedMessage =>
-                {
-                    var orders = binanceClient.GetCurrentOpenOrders().Result;
-                });
+            foreach (var order in trxOrders)
+            {
+                await _binanceClient.CancelOrder(symbol, order.OrderId);
+            }
 
-            var newOrder = binanceClient.PostNewOrder("ADAETH", 40, 90, OrderSide.SELL, OrderType.LIMIT, icebergQty:9).Result;
+            var newOrder = _binanceClient.PostNewOrder(symbol, 1, 90, OrderSide.SELL).Result;
+
+            //var priceChangeInfo = _binanceClient.GetPriceChange24H(symbol).Result;
+
+            //Console.WriteLine(priceChangeInfo.First().AskPrice);
         }
     }
 }
