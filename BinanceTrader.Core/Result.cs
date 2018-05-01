3febe3a5ffdff10c634be98e06db0cdea7d243bf
Rastorguev <rastorguevs@gmail.com>
2018-05-02 01:06:16 +0300

    Get min notional from trading rules

---
+++ BinanceTrader.Core/Result.cs
@@ -0,0 +1,14 @@
+﻿namespace BinanceTrader.Trader
+{
+    public class Result<T>
+    {
+        public bool Success { get; }
+        public T Value { get; }
+
+        public Result(bool success, T value)
+        {
+            Success = success;
+            Value = value;
+        }
+    }
+}
