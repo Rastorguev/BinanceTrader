0d1bf351ac3df607aa292af456a4bb24f96e5424
Rastorguev <rastorguevs@gmail.com>
2018-05-04 17:23:09 +0300

    Implement OrdersDistributer

---
+++ BinanceTrader.Core/OrdersDistributor.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using BinanceTrader.Tools;
+using JetBrains.Annotations;
+
+namespace BinanceTrader.Trader
+{
+    public static class OrdersDistributor
+    {
+        [NotNull]
+        public static Dictionary<string, List<decimal>> DistributeQuoteCurrency(
+            decimal freeQuoteAmount,
+            decimal minOrderSize,
+            [NotNull] Dictionary<string, int> openOrdersCount)
+        {
+            var ordersRequests = new Dictionary<string, List<decimal>>();
+            var remainingQuoteAmount = freeQuoteAmount;
+
+            while (true)
+            {
+                var minOrdersCountSymbols = openOrdersCount.Select(o =>
+                    {
+                        var requestsCount = ordersRequests.ContainsKey(o.Key.NotNull())
+                            ? ordersRequests[o.Key.NotNull()].NotNull().Count
+                            : 0;
+
+                        return (Symbol: o.Key, Count: requestsCount + o.Value);
+                    })
+                    .GroupBy(x => x.Count)
+                    .OrderBy(g => g.Key)
+                    .First().NotNull()
+                    .Select(g => g.Symbol)
+                    .ToList();
+
+                foreach (var symbol in minOrdersCountSymbols)
+                {
+                    if (remainingQuoteAmount < minOrderSize)
+                    {
+                        return ordersRequests;
+                    }
+
+                    if (!ordersRequests.ContainsKey(symbol.NotNull()))
+                    {
+                        ordersRequests[symbol] = new List<decimal>();
+                    }
+
+                    var orderSize = remainingQuoteAmount >= minOrderSize * 2 ? minOrderSize : remainingQuoteAmount;
+                    ordersRequests[symbol].NotNull().Add(orderSize);
+                    remainingQuoteAmount -= orderSize;
+                }
+            }
+        }
+
+        [NotNull]
+        public static List<decimal> DistributeBaseCurrency(
+            decimal freeBaseAmount,
+            decimal minOrderSize,
+            decimal price,
+            decimal stepSize)
+        {
+            var ordersRequests = new List<decimal>();
+            var remainingStepsAmount = (int) (freeBaseAmount / stepSize);
+
+            var minOrderSizeInSteps = (int) (minOrderSize / stepSize / price);
+            while (remainingStepsAmount >= minOrderSizeInSteps)
+            {
+                var orderSizeInSteps = remainingStepsAmount >= minOrderSizeInSteps * 2
+                    ? minOrderSizeInSteps
+                    : remainingStepsAmount;
+
+                ordersRequests.Add(orderSizeInSteps * stepSize);
+                remainingStepsAmount -= orderSizeInSteps;
+            }
+
+            return ordersRequests;
+        }
+    }
+}
