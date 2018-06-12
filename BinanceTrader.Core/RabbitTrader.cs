997499338ef5a38aa3972a4b50e0b0cc4e78a069
Rastorguev <rastorguevs@gmail.com>
2018-06-12 21:05:10 +0300

    Fix small issue

--- BinanceTrader.Core/RabbitTrader.cs
+++ BinanceTrader.Core/RabbitTrader.cs
@@ -389,7 +389,7 @@ namespace BinanceTrader.Trader
                 {
                     var result = (asset: c.symbol, fluctuation: 0m);
 
-                    if (!candles.Any())
+                    if (!c.candles.Any())
                     {
                         return result;
                     }
