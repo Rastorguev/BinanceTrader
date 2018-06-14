c2d0f0107c7e8e81a218bdd3733e09cd548ac811
Rastorguev <rastorguevs@gmail.com>
2018-06-14 20:34:11 +0300

    Change waiting time and profit ratio

--- BinanceTrader.Core/RabbitTrader.cs
+++ BinanceTrader.Core/RabbitTrader.cs
@@ -17,9 +17,9 @@ namespace BinanceTrader.Trader
     public class RabbitTrader
     {
         private readonly TimeSpan _scheduleInterval = TimeSpan.FromSeconds(20);
-        private readonly TimeSpan _sellWaitingTime = TimeSpan.FromHours(1);
-        private readonly TimeSpan _buyWaitingTime = TimeSpan.FromHours(1);
-        private const decimal ProfitRatio = 0.5m;
+        private readonly TimeSpan _sellWaitingTime = TimeSpan.FromHours(12);
+        private readonly TimeSpan _buyWaitingTime = TimeSpan.FromHours(12);
+        private const decimal ProfitRatio = 2m;
         private const string QuoteAsset = "ETH";
         private const string FeeAsset = "BNB";
 
