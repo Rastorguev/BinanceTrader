a3b628a62b82943133e3acdfa3e0a83bb768b2b9
Rastorguev <rastorguevs@gmail.com>
2018-07-07 10:28:31 +0300

    Log truncated key whe web job start requested

--- BinanceTrader.WebJob/Functions.cs
+++ BinanceTrader.WebJob/Functions.cs
@@ -1,10 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
-using System.Reflection;
 using Binance.API.Csharp.Client;
-using BinanceTrader.Tools;
 using BinanceTrader.Tools.KeyProviders;
 using BinanceTrader.Trader;
+using JetBrains.Annotations;
 using Microsoft.Azure.WebJobs;
 
 namespace BinanceTrader.WebJob
@@ -29,12 +29,33 @@ namespace BinanceTrader.WebJob
                 var binanceClient = new BinanceClient(apiClient);
 
                 var trader = new RabbitTrader(binanceClient, logger);
-                trader.Start();
+
+                logger.LogMessage("StartRequested", new Dictionary<string, string>
+                {
+                    {"Api", keys.Api != null ? GetTruncatedKey(keys.Api) : "Invalid Api Key"},
+                    {"Secret", keys.Secret != null ? GetTruncatedKey(keys.Secret) : "Invalid Secret Key"}
+                });
+
+                trader.Start().Wait();
             }
             catch (Exception ex)
             {
                 logger.LogException(ex);
             }
+        }
+
+        private static string GetTruncatedKey([NotNull] string key)
+        {
+            const int n = 5;
+
+            if (key.Length < n * 2 + 10)
+            {
+                return key;
+            }
+
+            var truncated = $"{key.Substring(0, n)}...{key.Substring(key.Length - n, n)}";
+
+            return truncated;
         }
     }
 }
