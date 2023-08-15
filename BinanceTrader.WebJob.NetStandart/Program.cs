d51443858d8054b7da97f4d44f5c72ecccda58cb
Aliaksandr Rastarhuyeu <alexanderra@playtika.com>
2023-08-15 09:59:55 +0200

    Add WebJob netStandart project

---
+++ BinanceTrader.WebJob.NetStandart/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+using System.Net;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Hosting;
+
+namespace BinanceTrader.WebJob
+{
+    internal class Program
+    {
+        static async Task Main()
+        {
+            ServicePointManager.DefaultConnectionLimit = 10;
+
+            CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
+            CultureInfo.CurrentUICulture = CultureInfo.InvariantCulture;
+
+            var builder = new HostBuilder();
+            builder.UseEnvironment("development");
+            builder.ConfigureWebJobs(b =>
+            {
+                 b.AddAzureStorageCoreServices();
+            });
+            var host = builder.Build();
+            using (host)
+            {
+                var jobHost = host.Services.GetService(typeof(IJobHost)) as JobHost;
+                await jobHost.CallAsync("Start");
+                await host.RunAsync();
+            }
+        }
+    }
+}
