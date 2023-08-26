919b7789c4b9c5bdf69c20f6662947f813c33100
Aliaksandr Rastarhuyeu <alexanderra@playtika.com>
2023-08-26 21:24:58 +0200

    Add JsonProperty annotations to Candlestick

--- Api/BinanceApi.Models/Market/Candlestick.cs
+++ Api/BinanceApi.Models/Market/Candlestick.cs
@@ -1,19 +1,24 @@
 ﻿using System;
 using BinanceApi.Models.Extensions;
+using Newtonsoft.Json;
 
 namespace BinanceApi.Models.Market
 {
     public class Candlestick
     {
+        [JsonProperty("openTime")]
         public long OpenUnixTime { get; set; }
+
+        [JsonProperty("closeTime")]
+        public long CloseUnixTime { get; set; }
+
         public DateTime OpenLocalTime => OpenUnixTime.GetLocalTime();
+        public DateTime CloseLocalTime => CloseUnixTime.GetLocalTime();
         public decimal Open { get; set; }
         public decimal High { get; set; }
         public decimal Low { get; set; }
         public decimal Close { get; set; }
         public decimal Volume { get; set; }
-        public long CloseUnixTime { get; set; }
-        public DateTime CloseLocalTime => CloseUnixTime.GetLocalTime();
         public decimal QuoteAssetVolume { get; set; }
         public int NumberOfTrades { get; set; }
         public decimal TakerBuyBaseAssetVolume { get; set; }
